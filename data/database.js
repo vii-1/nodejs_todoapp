76b51ebb47f459ceb7a0ab5f703b015ebc576a0c
Vedant Pawar <vedantpawar154@gmail.com>
2023-07-25 16:15:32 +0530

    final 6

--- data/database.js
+++ data/database.js
@@ -7,7 +7,7 @@ config({
 
 export const connectDB=()=>{
     console.log(process.env.MONGO_URI);
-    mongoose.connect(process.env.MONGO_URI,{
+    mongoose.connect("mongodb://new-user:newuser@ac-yowlkmi-shard-00-00.cvmqgmz.mongodb.net:27017,ac-yowlkmi-shard-00-01.cvmqgmz.mongodb.net:27017,ac-yowlkmi-shard-00-02.cvmqgmz.mongodb.net:27017/?ssl=true&replicaSet=atlas-hv9lyw-shard-0&authSource=admin&retryWrites=true&w=majority",{
     dbName:"Backendapi"
 })
 .then((c)=>console.log(`Database connected ${c.connection.host}`));
