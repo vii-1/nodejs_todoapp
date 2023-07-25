273fac429e58e06a1c50cc2fc7f65becf37f1a2d
Vedant Pawar <vedantpawar154@gmail.com>
2023-07-25 16:05:59 +0530

    Nakki

--- data/database.js
+++ data/database.js
@@ -1,12 +1,9 @@
 import mongoose from "mongoose";
-
 import { config } from "dotenv";
 
 config({
-    path:"config.env",
+    path:"./data/config.env",
 });
-
-
 
 export const connectDB=()=>{
     mongoose.connect(process.env.MONGO_URI,{
